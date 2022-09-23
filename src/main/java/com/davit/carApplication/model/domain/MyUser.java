1657127b4e2b64c3d15b8b377ead843157982e70
Davit akhvlediani <davidakhvlediani7@gmail.com>
2022-09-23 14:13:07 +0400

    add repositories

---
+++ src/main/java/com/davit/carApplication/model/domain/MyUser.java
@@ -0,0 +1,19 @@
+package com.davit.carApplication.model.domain;
+
+import com.davit.carApplication.model.enums.UserStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+}
---
+++ src/main/java/com/davit/carApplication/model/enums/UserStatus.java
@@ -0,0 +1,5 @@
+package com.davit.carApplication.model.enums;
+
+public enum UserStatus {
+    ACTIVE,BANNED
+}
