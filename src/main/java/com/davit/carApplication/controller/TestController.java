b437467cf573b2e34a1da5762b6405e5c4c58ff9
Davit akhvlediani <davidakhvlediani7@gmail.com>
2022-09-23 12:39:44 +0400

    my first commit

---
+++ src/main/java/com/davit/carApplication/controller/TestController.java
@@ -0,0 +1,4 @@
+package com.davit.carApplication.controller;
+
+public class TestController {
+}
---
+++ src/test/java/com/davit/carApplication/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davit.carApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
